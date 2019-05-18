86af9d411ec9b92e890873b55256c8a2dd1612ec
Alan Anderson <alan.anderson@udacity.com>
2019-05-18 11:46:24 -0700

    Add P1 LruCache -- Not working

---
+++ P1/lrucache.py
@@ -0,0 +1,45 @@
+from collections import OrderedDict
+
+class LRU_Cache(object):
+    def __init__(self, capacity):
+        # Initialize class variables
+        self.capacity = capacity
+        self.memory = OrderedDict()
+
+    def get(self, key):
+        # mark the element as MRU
+        if key not in self.memory:
+            return -1
+        value = self.memory[key]
+        del(self.memory[key])
+        self.memory[key] = value
+        return value
+
+    def set(self, key, value):
+        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item. 
+        if len(self.memory) ==  self.capacity:
+            # clear LRU slot
+            self.memory.popitem()
+        self.memory[key] = value
+
+our_cache = LRU_Cache(5)
+
+our_cache.set(1, 1)
+our_cache.set(2, 2)
+our_cache.get(1)       # returns 1
+our_cache.get(2)       # returns 2
+our_cache.get(3)       # return -1
+
+def test_lru():
+    cache = LRU_Cache(3)
+    cache.set(1, 1)
+    cache.set(2, 2)
+    cache.set(3, 3)
+    assert cache.get(2) == 2
+    cache.set(4, 4) # add 4 remove 1
+    assert cache.get(1) == -1
+    cache.set(5, 5) # add 5 remove 3
+    assert cache.get(3) == -1
+    assert cache.get(4) == 4
+
+test_lru()
